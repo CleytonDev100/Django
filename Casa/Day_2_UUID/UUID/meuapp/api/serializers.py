592766439f3b94e021f16b40e550e0e97de87bb7
Cleyton Bezerra Miguel <116188906+CleytonDev100@users.noreply.github.com>
2023-04-13 19:49:25 -0300

    Delete Casa/Day_2_UUID directory

--- Casa/Day_2_UUID/UUID/meuapp/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from meuapp import models
-
-class Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Cliente
-        fields = "__all__"
